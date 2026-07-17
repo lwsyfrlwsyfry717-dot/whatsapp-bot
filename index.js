c237283eb1a04fd78c44fc9d403b7ce383b8324c
lwsyfrlwsyfry717-dot <lwsyfrlwsyfry717@gmail.com>
2026-07-17 09:22:32 +0300

    show qr

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {
 
 const pino = require("pino");
 const http = require("http");
+const qrcode = require("qrcode-terminal");
 
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("session");
@@ -13,14 +14,16 @@ async function startBot() {
   const sock = makeWASocket({
     auth: state,
     logger: pino({ level: "silent" }),
-    printQRInTerminal: true
+    printQRInTerminal: false
   });
 
   sock.ev.on("creds.update", saveCreds);
 
   sock.ev.on("connection.update", ({ connection, qr, lastDisconnect }) => {
+
     if (qr) {
-      console.log("📱 امسح رمز QR الذي يظهر في السجل.");
+      console.log("📱 امسح رمز QR التالي:");
+      qrcode.generate(qr, { small: true });
     }
 
     if (connection === "open") {
@@ -31,6 +34,8 @@ async function startBot() {
       const shouldReconnect =
         lastDisconnect?.error?.output?.statusCode !==
         DisconnectReason.loggedOut;
+
+      console.log("❌ انقطع الاتصال");
 
       if (shouldReconnect) {
         startBot();
@@ -44,6 +49,7 @@ startBot();
 const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("WhatsApp Bot is Running");
 }).listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
